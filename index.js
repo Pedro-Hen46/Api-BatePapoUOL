a3bb04430bcafd9162ec316ea29da7a61d6347c2
Pedro Henrique <driverique@gmail.com>
2022-06-25 22:07:42 -0300

    POST/Participants on MongoDB - OK

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import chalk from 'chalk';
+import joi from 'joi';
+import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
-import { ClientRequest } from 'http';
 
 dotenv.config();
 
 const server = express();
+server.use(cors());
 server.use(express.json());
+
+
+const SERVER_PORT = process.env.SERVER_PORT;
 
 const Client = new MongoClient(process.env.MONGO_URL);
 let db;
 
-Client.connect(() => {
-    db = Client.db("api_uol");
+
+Client.connect().then(() => {
+    db = Client.db("api_uol_batepapo");
 })
+
+const participantsSchema = joi.object({
+    name: joi.string().required()
+});
 
 
 server.post('/participants', async (req, res) => {
     const name = req.body;
+    
+    const validation = participantsSchema.validate(name, {abortEarly: true});
+    
+    if(validation.error){
+        const {error} = validation;
+        const message_error = error.details.map(item => item.message);
+        
+        res.status(422).send(message_error);
+        return;
+    }
+    
+    const lastStatus = Date.now();
+    const participant = { name, lastStatus};
 
-    const teste = await db.collections('participants').createOne()
+    await db.collection('participants').insertOne(participant);
+
+    // await db.collection("participants").insertOne({
+    //     name: "Osvaldo"
+    // });
+
+    res.status(201).send();
 });
 
 
@@ -28,6 +59,6 @@ server.post('/participants', async (req, res) => {
 
 
 
-server.listen(5000, () => {
-    console.log("Servidor iniciado na porta: 5000");
+server.listen(SERVER_PORT, () => {
+    console.log(chalk.yellow.bold(`Servidor iniciado na porta: ${SERVER_PORT}`));
 });
